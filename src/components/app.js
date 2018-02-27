bcf2f3a6ef8e758ebed78b1b1ff140d3d0232afa
Thomas Miller <thomasmiller@Thomass-MacBook-Pro.local>
2018-02-28 06:11:27 +1000

    fin42 implementation of container

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple startsadsfer</div>
+      <div>
+        
+        <BookList />
+        <div className="col-sm-12" >
+        <h1>this is headding 1</h1>
+        </div>
+      </div>
     );
   }
 }
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,20 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect }from 'react-redux';
 
-export default class BookList extends Component {
-    renderList () {
+export class BookList extends Component {
+    renderList() {
         return this.props.books.map((book) => {
             return (
-            <li key={book.title} className="list-group-item"> {book.title} </li>
+                <li key={book.title} className="list-group-item"> {book.title} </li>
             );
         });
     }
 
     render() {
         return (
+
             <ul className="list-group col-sm-4">
                 {this.renderList()}
             </ul>
 
+
+
+
         );
     }
 }
+
+function mapStateToProps(state) {
+    // Whatever is returned will show up as props
+    // inside of BookList
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
